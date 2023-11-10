f22a136790a47d952407236c4890afaaf9849764
Xavier <91749310+arseniox@users.noreply.github.com>
2023-11-10 18:55:15 +0100

    New name

--- APIManagement.py
+++ APIManagement.py
@@ -41,7 +41,7 @@ async def fetch_data():
 
 if __name__ == "__main__":
     print("-> Inicio integrado de servicio web")
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload="True")
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
 
     
 else:
